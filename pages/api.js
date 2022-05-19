91ab1594b6f6674d39a76029e988f2f47672a2e9
Vlad Skoromnyi <99530193+VladSkoromnyi@users.noreply.github.com>
2022-05-19 14:26:34 +0300

    Delete api.js

--- pages/api.js
+++
@@ -1,6 +0,0 @@
-const BASE_URL = 'https://jsonplaceholder.typicode.com';
-
-export const dataFromServer = async () => {
-	const response = await fetch(`${BASE_URL}/posts`);
-	return await response.json();
-}
